a882c871b4cc009b514c6b5c3124b1318dc6f63f
Willian Adriano <71937155+WillianAUK@users.noreply.github.com>
2021-10-20 21:34:37 -0300

    Update Codigo_final_WillianAuk.ino

--- Sistemas embarcados/Codigo final/codigo_final/Codigo_final_WillianAuk.ino
+++ Sistemas embarcados/Codigo final/codigo_final/Codigo_final_WillianAuk.ino
@@ -1,18 +1,20 @@
 // biblioteca  arduino para display
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(7, 6, 5, 4, 3, 2);  // Pinos display
-const int potenciometro = 0;          // pino de entrada do potenciômetro
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);     // Pinos display
+const int potenciometro = 0;             // pino de entrada do potenciômetro
 int valor = 0;
+float saida = 0;
 
 void setup() {
   lcd.begin(16, 2);
-  lcd.print("IFPB - SE_2021.1");      // mensagem display
+  lcd.print("IFPB - SE_2021.1");         // mensagem display
 }
 
 void loop() {
   lcd.setCursor(0, 1);
-  valor = analogRead(potenciometro);  // recebendo valor
-  lcd.print(valor);                   // mostrando valor no display
-  delay(10);                          // tempo para nova leitura
+  valor = analogRead(potenciometro);     // recebendo valor
+  saida = valor * 0.00489;               // convertendo pra volts
+  lcd.print(saida, 2);                   // mostrando valor no display com duas casas decimais
+  delay(10);                             // tempo para nova leitura
 }
